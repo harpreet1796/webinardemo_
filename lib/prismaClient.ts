6fb7ba6eaf47887758d29ccb3f3f9d20e59c20ee
Harpreet Singh <harpreetofficial2009@gmail.com>
2026-06-23 12:31:39 +0530

    fix: migrate configuration to prisma 7 standards

--- lib/prismaClient.ts
+++ lib/prismaClient.ts
@@ -1,4 +1,9 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
+
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg(pool)
 
 declare global {
     // allow global `var` declarations
@@ -7,5 +12,5 @@ declare global {
 }
 
 export const prismaClient = globalThis.prisma || new PrismaClient();
-
+export const prisma = new PrismaClient({ adapter })
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prismaClient;
